4c178bb795a7fbbbb568c5586979ceaddf9028a2
DarkDev <darkdev_brand@riseup.net>
2024-01-01 18:22:17 +0200

    add hero

---
+++ src/lib/widgets/ui/landing/Hero.svelte
@@ -0,0 +1,7 @@
+<section class="main-bg h-auto w-full ">
+	<img src="./images/hero.svg" class="h-full w-full" alt="" />
+
+	<div class="mx-auto h-screen h-full max-w-4xl rounded-2xl bg-white pt-6">
+		<h1 class="text-center text-6xl font-black text-black">Made by utopia</h1>
+	</div>
+</section>
