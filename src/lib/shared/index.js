b39efe3de497da53fe623d475bac2fe92868bde5
DarkDev <darkdev_brand@riseup.net>
2024-01-01 18:21:51 +0200

    upd api

---
+++ src/lib/shared/index.js
@@ -0,0 +1,4 @@
+import countries from './data/countries.js';
+import persons from './data/persons.js';
+
+export { countries, persons };
---
+++ src/lib/widgets/index.js
@@ -0,0 +1,11 @@
+import Countries from './ui/landing/Countries.svelte';
+import Aflag from './ui/landing/Aflag.svelte';
+import Footer from './ui/landing/Footer.svelte';
+import Header from './ui/landing/Header.svelte';
+import Hero from './ui/landing/Hero.svelte';
+import OtherGames from './ui/landing/OtherGames.svelte';
+import SupportUs from './ui/landing/SupportUs.svelte';
+
+
+
+export { Countries, Aflag, Footer, Header, Hero, OtherGames, SupportUs };
