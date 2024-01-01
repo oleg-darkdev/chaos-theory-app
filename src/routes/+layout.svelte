c44f221ee3351b83f1aa2d571e5827e590be3be4
DarkDev <darkdev_brand@riseup.net>
2024-01-01 18:22:30 +0200

    basic layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import AOS from 'aos';
+	import 'aos/dist/aos.css';
+	import { onMount } from 'svelte';
+	import '../app.css';
+	onMount(() => {
+		AOS.init();
+	});
+</script>
+
+<main class="main-bg">
+	<slot />
+</main>
+
+<style>
+  :global(.main-bg {
+    background-color: #080606;
+  })
+</style>
