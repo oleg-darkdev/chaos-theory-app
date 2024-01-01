5ac9560527b04dfdd8d54babfea69c45ebdd325a
DarkDev <darkdev_brand@riseup.net>
2024-01-01 18:21:30 +0200

    add countries list

---
+++ src/lib/widgets/ui/landing/Countries.svelte
@@ -0,0 +1,26 @@
+<script>
+	import { countries } from '$lib/shared/';
+</script>
+
+<section class="w-full bg-black ">
+	<!-- rounded-2xl border-4 border-neutral-400  -->
+	<div
+		class="mx-auto  max-w-6xl px-8 py-12"
+		style="background-image: url(./images/countries-bg.svg)"
+	>
+		<h2 class="mb-6 text-center text-6xl font-black text-neutral-400">Extensions by countries</h2>
+		<div class="mx-auto grid max-w-5xl grid-cols-2 justify-items-center gap-4 ">
+			{#each countries as country}
+				<div
+					class="group flex w-full max-w-sm flex-row flex-wrap items-center border-2 border-white  bg-black py-4 px-2 hover:bg-white "
+				>
+					<img src="./images/flags/{country.flag}" class="h-24 w-24 lg:mr-4" alt={country.title} />
+
+					<h3 class="text-5xl font-bold text-white group-hover:text-black">
+						{country.title}
+					</h3>
+				</div>
+			{/each}
+		</div>
+	</div>
+</section>
