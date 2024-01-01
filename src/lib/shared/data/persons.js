566c7288a9de9d837d852f50287644024b7b4eea
DarkDev <darkdev_brand@riseup.net>
2024-01-01 18:23:37 +0200

    alfa v.

---
+++ src/lib/shared/data/persons.js
@@ -0,0 +1,73 @@
+const persons = [
+	{
+		title: 'Mutual',
+		link: '',
+		desc: [''],
+		img: 'mutual.svg',
+		bgColor: '#e29902'
+	},
+	{
+		title: 'Capital',
+		link: '',
+		desc: [''],
+		img: 'capital.svg',
+		bgColor: '#ece326'
+	},
+
+	{
+		title: 'Ekolog',
+		link: '',
+		desc: [''],
+		img: 'ekolog.svg',
+		bgColor: '#109a39'
+	},
+	{
+		title: 'Individual',
+		link: '',
+		desc: [''],
+		img: 'individual.svg',
+		bgColor: '#057c69'
+	},
+	{
+		title: 'Quir',
+		link: '',
+		desc: [''],
+		img: 'quir.svg',
+		bgColor: '#dc88b8'
+	},
+	{
+		title: 'Feminist',
+		link: '',
+		desc: [''],
+		img: 'feminist.svg',
+		bgColor: '#913b8e'
+	},
+	{
+		title: 'Techno',
+		link: '',
+		desc: [''],
+		img: 'techno.svg',
+		bgColor: '#366eb5'
+	},
+
+	{
+		title: 'Max',
+		link: '',
+		desc: [''],
+		img: 'max.svg',
+		bgColor: '#df3814'
+	},
+
+	{
+		title: 'Sindikat',
+		link: '',
+		desc: [''],
+		img: 'sindikat.svg',
+		bgColor: '#df3814'
+	},
+
+
+  
+];
+
+export default persons;
