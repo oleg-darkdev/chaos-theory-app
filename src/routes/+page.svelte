a463e4550d2c401a565fc82205674000616f9928
DarkDev <darkdev_brand@riseup.net>
2024-01-01 18:22:40 +0200

    beta v.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { persons } from '$lib/shared';
+	import { Aflag, Countries, Footer, Header, Hero, SupportUs, OtherGames } from '$lib/widgets';
+</script>
+
+<Header />
+
+<Hero />
+
+<Aflag {persons} />
+
+<SupportUs />
+
+<Countries />
+
+<OtherGames />
+
+<Footer />
