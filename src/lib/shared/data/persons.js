fc1d9037848773aeb2651e15f65b04ecdb70c2ef
DarkDev <darkdev_brand@riseup.net>
2024-01-02 17:56:58 +0200

    upd info

--- src/lib/shared/data/persons.js
+++ src/lib/shared/data/persons.js
@@ -1,73 +1,106 @@
 const persons = [
 	{
 		title: 'Mutual',
-		link: '',
-		desc: [''],
+		link: '/',
+		desc: [
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+		],
 		img: 'mutual.svg',
-		bgColor: '#e29902'
+		bgColor: '#e29902',
+		altText: 'Mutual anarchism'
 	},
 	{
 		title: 'Capital',
-		link: '',
-		desc: [''],
+		link: '/',
+		desc: [
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+		],
 		img: 'capital.svg',
-		bgColor: '#ece326'
+		bgColor: '#ece326',
+		altText: 'Anarcho capitalism'
 	},
 
 	{
 		title: 'Ekolog',
-		link: '',
-		desc: [''],
+		link: '/',
+		desc: [
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+		],
 		img: 'ekolog.svg',
-		bgColor: '#109a39'
+		bgColor: '#109a39',
+		altText: 'Eco anarchism'
 	},
 	{
 		title: 'Individual',
-		link: '',
-		desc: [''],
+		link: '/',
+		desc: [
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+		],
 		img: 'individual.svg',
-		bgColor: '#057c69'
+		bgColor: '#057c69',
+		altText: 'Anarcho individualism'
 	},
 	{
 		title: 'Quir',
-		link: '',
-		desc: [''],
+		link: '/',
+		desc: [
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+		],
 		img: 'quir.svg',
-		bgColor: '#dc88b8'
+		bgColor: '#dc88b8',
+		altText: 'Quir anarchism'
 	},
 	{
 		title: 'Feminist',
-		link: '',
-		desc: [''],
+		link: '/',
+		desc: [
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+		],
 		img: 'feminist.svg',
-		bgColor: '#913b8e'
+		bgColor: '#913b8e',
+		altText: 'Femism anarchism'
 	},
 	{
 		title: 'Techno',
-		link: '',
-		desc: [''],
+		link: '/',
+		desc: [
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+		],
 		img: 'techno.svg',
-		bgColor: '#366eb5'
+		bgColor: '#366eb5',
+		altText: 'Techno anarchism'
 	},
 
 	{
 		title: 'Max',
-		link: '',
-		desc: [''],
+		link: '/',
+		desc: [
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+		],
 		img: 'max.svg',
-		bgColor: '#df3814'
+		bgColor: '#df3814',
+		altText: ''
 	},
 
 	{
 		title: 'Sindikat',
-		link: '',
-		desc: [''],
+		link: '/',
+		desc: [
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+		],
 		img: 'sindikat.svg',
-		bgColor: '#df3814'
-	},
-
-
-  
+		bgColor: '#df3814',
+		altText: 'Anarcho sindikalism'
+	}
 ];
 
 export default persons;
