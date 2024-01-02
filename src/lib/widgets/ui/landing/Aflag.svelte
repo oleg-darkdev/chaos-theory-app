80033b86a37036087c119fd80609154ef1bbffa8
DarkDev <darkdev_brand@riseup.net>
2024-01-02 17:57:47 +0200

    refactoring text blocks

--- src/lib/widgets/ui/landing/Aflag.svelte
+++ src/lib/widgets/ui/landing/Aflag.svelte
@@ -6,12 +6,25 @@
 	{#each persons as person}
 		<div class="group mx-auto flex h-auto w-full max-w-6xl flex-row  items-center justify-center ">
 			<div class="h-screen w-6/12 border-r-4 px-10  py-16" style="border-color: {person.bgColor};">
-				<h2 class="text-6xl font-black  " style="color: {person.bgColor};">{person.title}</h2>
-<!-- сделать большими буквами, с расстоянием -->
-				<span class="text-xl font-light  " style="color: {person.bgColor};">anarchist </span>
-				{#each person.desc as desc}
-					<p style="color: {person.bgColor};">{person.desc}</p>
-				{/each}
+				<h2 class="mb-2 text-6xl font-black  " style="color: {person.bgColor};">{person.title}</h2>
+				<span
+					class=" text-xl font-semibold uppercase  tracking-widest "
+					style="color: {person.bgColor};"
+					>anarchist
+				</span>
+				<div class="mt-6 mb-20 max-w-sm">
+					{#each person.desc as desc}
+						<p style="color: {person.bgColor}; " class='mb-2'>{person.desc}</p>
+					{/each}
+				</div>
+				<div class="">
+					<a
+						href={person.link}
+						style="background-color: {person.bgColor}; "
+						class="hover:white flex w-full transform items-center justify-center  rounded-md px-10 py-4 text-center text-base font-medium text-white transition duration-500 ease-in-out "
+						>More about this philosophy of anarchism
+					</a>
+				</div>
 			</div>
 			<div
 				class="flex w-5/12 flex-col rounded-br-2xl rounded-tr-2xl  border-4 bg-black"
