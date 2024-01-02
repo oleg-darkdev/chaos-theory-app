f6749a2b9d9a91654ee035dc2ec8ef86a2356bc8
DarkDev <darkdev_brand@riseup.net>
2024-01-02 17:57:14 +0200

    add test data

---
+++ src/lib/shared/data/otherBoardgames.js
@@ -0,0 +1,74 @@
+const games = [
+	{
+		title: 'LGBT',
+		desc: [
+			'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+			''
+		],
+		img: 'https://miro.medium.com/max/2400/0*hDAyhnOx767w5qma.jpg',
+		link: '/',
+		price: '30'
+	},
+	{
+		title: 'Skquat wars',
+		desc: [
+			'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+			''
+		],
+		img: 'https://miro.medium.com/max/2400/0*hDAyhnOx767w5qma.jpg',
+		link: '/',
+		price: '30'
+	},
+	{
+		title: 'Food not Bombs',
+		desc: [
+			'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+			''
+		],
+		img: 'https://miro.medium.com/max/2400/0*hDAyhnOx767w5qma.jpg',
+		link: '/',
+		price: '30'
+	},
+	{
+		title: 'Rhytms of resistanse',
+		desc: [
+			'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+			''
+		],
+		img: 'https://miro.medium.com/max/2400/0*hDAyhnOx767w5qma.jpg',
+		link: '/',
+		price: '30'
+	},
+	{
+		title: 'Graffity wars',
+		desc: [
+			'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+			''
+		],
+		img: 'https://miro.medium.com/max/2400/0*hDAyhnOx767w5qma.jpg',
+		link: '/',
+		price: '30'
+	},
+	{
+		title: 'Locators wars',
+		desc: [
+			'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+			''
+		],
+		img: 'https://miro.medium.com/max/2400/0*hDAyhnOx767w5qma.jpg',
+		link: '/',
+		price: '30'
+	},
+	{
+		title: 'Eco activism',
+		desc: [
+			'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+			''
+		],
+		img: 'https://miro.medium.com/max/2400/0*hDAyhnOx767w5qma.jpg',
+		link: '/',
+		price: '30'
+	}
+];
+
+export default games;
