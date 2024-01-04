b5db2cb73e7a4df72ca939b68b52a4f1bc4a7234
DarkDev <darkdev_brand@riseup.net>
2024-01-04 19:11:12 +0200

    intial

---
+++ src/lib/shared/data/app/headerLinks.js
@@ -0,0 +1,10 @@
+const headerLinks = [
+	{ link: '/app', img: 'header_app.svg', text: 'App' },
+	{ link: '/app/extensions', img: 'header_extensions.svg', text: 'Extensions' },
+	{ link: '/app/manual/locations', img: 'header_locations.svg', text: 'Rooms' },
+	{ link: '/app/manual/experiments', img: 'header_experiments.svg', text: 'Experiments' },
+	{ link: '/app/manual/persons', img: 'header_сharacters.svg', text: 'Persons' },
+	{ link: '/synopsis', img: 'header_synopsis.svg', text: 'Synopsis' }
+];
+
+export default headerLinks;
