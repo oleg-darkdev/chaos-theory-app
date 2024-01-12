ccbb2541cce26e2162302d0b8701447266ce904d
DarkDev <darkdev_brand@riseup.net>
2024-01-13 01:12:15 +0200

    add anchor

--- src/lib/widgets/ui/landing/Hero.svelte
+++ src/lib/widgets/ui/landing/Hero.svelte
@@ -2,6 +2,8 @@
 	<img src="./images/hero.svg" class="h-full w-full" alt="" />
 
 	<div class="mx-auto h-screen h-full max-w-4xl rounded-2xl bg-white pt-6">
+		<a name="about" />
+
 		<h1 class="text-center text-6xl font-black text-black">Made by utopia</h1>
 	</div>
 </section>
