ce247940edf3c55ac820aba291ac4738a884660b
DarkDev <darkdev_brand@riseup.net>
2024-01-13 01:12:27 +0200

    add anchor & subtitle

--- src/lib/widgets/ui/landing/SupportUs.svelte
+++ src/lib/widgets/ui/landing/SupportUs.svelte
@@ -1,9 +1,14 @@
+<a name="donate" />
+
 <section class="mb-20 w-full">
 	<div class="main-bg mx-auto flex max-w-6xl flex-row rounded-xl border-2 border-[#df3814]">
 		<img src="./images/subscribe.svg" class="h-full w-8/12 rounded-lg" alt="" />
 
 		<div class="">
-			<h2>Support us</h2>
+			<h2 class="mb-2 text-6xl font-black">Support the progect</h2>
+			<span class="text-md font-semibold uppercase  tracking-widest "
+				>Lorem ipsum dolor sit amet.
+			</span>
 			<div class="">Patronite</div>
 
 			<div class="flex flex-row">
