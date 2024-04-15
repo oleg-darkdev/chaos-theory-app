d95410791615cc8108274b1b3501a2eeae33cd66
DarkDev <darkdev_brand@riseup.net>
2024-04-15 15:25:17 +0300

    upd api

--- src/lib/shared/data/persons/index.js
+++ src/lib/shared/data/persons/index.js
@@ -8,85 +8,97 @@ const persons = [
 		],
 		img: 'christian.svg',
 		bgColor: '#ffffff',
-		altText: 'Mutual anarchism'
+		altText: 'Mutual anarchism',
+		wiki: ''
 	},
 	{
 		title: 'Mutual',
 		link: '/',
 		desc: [
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+			`Mutualism is an anarchist school of thought and economic theory that advocates for workers' control of the means of production, a market economy made up of individual artisans and workers' cooperatives, and occupation and use property rights.`,
+			`As proponents of the labour theory of value and labour theory of property, mutualists oppose all forms of economic rent, profit and non-nominal interest, which they see as relying on the exploitation of labour. Mutualists seek to construct an economy without capital accumulation or concentration of land ownership.`
 		],
 		img: 'mutual.svg',
 		bgColor: '#e29902',
-		altText: 'Mutual anarchism'
+		altText: 'Mutual anarchism',
+		wiki: 'https://en.wikipedia.org/wiki/Mutualism_(economic_theory)'
 	},
 	{
 		title: 'Capital',
 		link: '/',
 		desc: [
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+			`Anarcho-capitalism is an anti-statist, libertarian political philosophy and economic theory that seeks to abolish centralized states in favor of stateless societies with systems of private property enforced by private agencies, based on concepts such as the non-aggression principle, free markets and self-ownership.`,
+			`In the absence of statute, anarcho-capitalists hold that society tends to contractually self-regulate and civilize through participation in the free market, which they describe as a voluntary society involving the voluntary exchange of goods and services.`
 		],
 		img: 'capital.svg',
 		bgColor: '#ece326',
-		altText: 'Anarcho capitalism'
+		altText: 'Anarcho capitalism',
+		wiki: 'https://en.wikipedia.org/wiki/Anarcho-capitalism'
 	},
 
 	{
 		title: 'Ekolog',
 		link: '/',
 		desc: [
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+			`Thought that focuses on ecology and environmental issues. It is an anti-capitalist and anti-authoritarian form of radical environmentalism, which emphasises social organization, freedom and self-fulfillment.`,
+			`A green anarchist theory is normally one that extends anarchism beyond a critique of human interactions and includes a critique of the interactions between humans and non-humans as well. Beyond human liberation, green anarchist praxis can extend to some form of non-human, total liberation, and environmentally sustainable anarchist society.`,
+			`The main tendencies of green anarchism are: social ecology, which argues that environmental issues stem directly from social issues; deep ecology, which critiques anthropocentrism and advocates instead for biocentrism; and anarcho-primitivism, which advocates for the abolition of technology and civilization.`
 		],
 		img: 'ekolog.svg',
 		bgColor: '#109a39',
-		altText: 'Eco anarchism'
+		altText: 'Eco anarchism',
+		wiki: ''
 	},
 	{
 		title: 'Individual',
 		link: '/',
 		desc: [
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+			`Individualist anarchism emphasizes the individual and their will over external determinants such as groups, society, traditions, and ideological systems. Although usually contrasted with social anarchism, both individualist and social anarchism have influenced each other. `,
+			`Economically, while European individualist anarchists are pluralists who advocate anarchism without adjectives and synthesis anarchism, ranging from anarcho-communist to mutualist economic types, most American individualist anarchists of the 19th century advocated mutualism, a libertarian socialist form of market socialism, or a free-market socialist form of classical economics.`
+
+			// `Egoist anarchism or anarcho-egoism, often shortened as simply egoism, is a school of anarchist thought that originated in the philosophy of Max Stirner, a 19th-century philosopher whose.`,
+			// `Egoist anarchism places the individual at the forefront, crafting ethical standards and actions based on this premise. It advocates personal liberation and rejects subordination, emphasizing the absolute priority of self-interest. `,
+			// `Max Stirner proposes that most commonly accepted social institutions—including the notion of State, property as a right, natural rights in general and the very notion of society—were mere phantasms or "spooks" in the mind. Stirner wanted to "abolish not only the state but also society as an institution responsible for its members".`
 		],
 		img: 'individual.svg',
 		bgColor: '#057c69',
-		altText: 'Anarcho individualism'
+		altText: 'Anarcho individualism',
+		wiki: 'https://en.wikipedia.org/wiki/Individualist_anarchism'
 	},
 	{
 		title: 'Quir',
 		link: '/',
 		desc: [
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+			`Queer anarchist that advocates anarchism and social revolution as a means of queer liberation and abolition of hierarchies such as homophobia, lesbophobia, transmisogyny, biphobia, transphobia, aphobia, heteronormativity, patriarchy, and the gender binary. `,
+			`Anarchism's foregrounding of individual freedoms made for a natural defense of homosexuality in the eyes of many, both inside and outside of the anarchist movement.`
 		],
 		img: 'quir.svg',
 		bgColor: '#dc88b8',
-		altText: 'Quir anarchism'
+		altText: 'Quir anarchism',
+		wiki: 'https://en.wikipedia.org/wiki/Queer_anarchism'
 	},
 	{
 		title: 'Feminist',
 		link: '/',
 		desc: [
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+			`Anarcha-feminism first emerged as a political current at a time when gender inequality was systematically enforced and women were excluded from public life.`,
+			`Their existence was confined to the traditional gender roles of mothers and wives, within the construct of the nuclear family. In particular, working class women were both politically and economically disenfranchised, which drove them closer to socialism and political militancy. `
 		],
 		img: 'feminist.svg',
 		bgColor: '#913b8e',
-		altText: 'Femism anarchism'
+		altText: 'Femism anarchism',
+		wiki: 'https://en.wikipedia.org/wiki/Anarcha-feminism'
 	},
 	{
 		title: 'Techno',
 		link: '/',
 		desc: [
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+			`Techno-anarchists propose a society in which technology empowers individuals and communities to achieve self-governance, decentralization, and environmental sustainability. By blending the principles of anarchism with technological innovation, we seek to create a world where freedom, autonomy, and cooperation are the cornerstones of our social and economic systems.`
 		],
 		img: 'techno.svg',
 		bgColor: '#366eb5',
-		altText: 'Techno anarchism'
+		altText: 'Techno anarchism',
+		wiki: ''
 	},
 
 	{
@@ -98,19 +110,22 @@ const persons = [
 		],
 		img: 'max.svg',
 		bgColor: '#df3814',
-		altText: ''
+		altText: '',
+		wiki: ''
 	},
 
 	{
 		title: 'Sindikat',
 		link: '/',
 		desc: [
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+			`Anarcho-syndicalist theory generally focuses on the labour movement.`,
+			`Anarcho-syndicalism is a political philosophy of thought that views revolutionary industrial unionism or syndicalism as a method for workers in capitalist society to gain control of an economy and thus control influence in broader society.`,
+			`The roots of anarcho-syndicalism lie in the anarchist faction of the International Workingmen's Association (IWA), which upheld the central role of trade unions in the class struggle and called for a general strike to replace the state with a free association of producers. `
 		],
 		img: 'sindikat.svg',
 		bgColor: '#df3814',
-		altText: 'Anarcho sindikalism'
+		altText: 'Anarcho sindikalism',
+		wiki: 'https://en.wikipedia.org/wiki/Anarcho-syndicalism'
 	}
 ];
 
