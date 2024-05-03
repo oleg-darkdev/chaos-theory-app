f62609fd1e696910c342a536adf31c3f0c1cbd86
DarkDev <darkdev_brand@riseup.net>
2024-05-03 03:40:17 +0300

    add animation, refactoring

--- src/lib/widgets/ui/landing/Hero.svelte
+++ src/lib/widgets/ui/landing/Hero.svelte
@@ -1,4 +1,4 @@
-<section class="main-bg h-auto w-full ">
+<!-- <section class="main-bg h-auto w-full ">
 	<img src="./images/hero.svg" class="h-full w-full" alt="" />
 
 	<div class="mx-auto h-screen h-full max-w-4xl rounded-2xl bg-white pt-6">
@@ -6,4 +6,175 @@
 
 		<h1 class="text-center text-6xl font-black text-black">Made by utopia</h1>
 	</div>
+</section> -->
+
+<section class=''>
+	<svg
+		id="Layer_1"
+		data-name="Layer 1"
+		fill="#080606"
+		class=""
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 1000 1000"
+	>
+		<!-- <defs>
+    <clipPath id="world">
+      <circle cx="500" cy="500" r="450" stroke="none" fill="none" />
+    </clipPath>
+  </defs> -->
+		<!-- clip-path="url(#world)" -->
+		<g   order="2">
+			<rect height="1000" width="1000" class="" />
+			<g class="flame2">
+				<path
+					class="flame"
+					d="M-1000,1000V388c86-2,111-38,187-38s108,38,187,38,111-38,187-38,108,38,187,38,111-38,187-38,108,38,187,38,111-38,187-38,109,38,188,38,110-38,187-38,108,38,187,38,111-38,187-38,108,38,187,38,111-38,187-38,108,38,187,38,111-38,187-38,109,38,188,38c0,96,0,612,0,612Z"
+				/>
+				<path
+					class=" flame-mask"
+					d="M-1000,1000V388c86-2,111-38,187-38s108,38,187,38,111-38,187-38,108,38,187,38,111-38,187-38,108,38,187,38,111-38,187-38,109,38,188,38,110-38,187-38,108,38,187,38,111-38,187-38,108,38,187,38,111-38,187-38,108,38,187,38,111-38,187-38,109,38,188,38c0,96,0,612,0,612Z"
+				/>
+			</g>
+			<g class="flame1">
+				<path
+					class="flame"
+					d="M-1000,1000V388c86-2,111-38,187-38s108,38,187,38,111-38,187-38,108,38,187,38,111-38,187-38,108,38,187,38,111-38,187-38,109,38,188,38,110-38,187-38,108,38,187,38,111-38,187-38,108,38,187,38,111-38,187-38,108,38,187,38,111-38,187-38,109,38,188,38c0,96,0,612,0,612Z"
+				/>
+				<path
+					class=" flame-mask"
+					d="M-1000,1000V388c86-2,111-38,187-38s108,38,187,38,111-38,187-38,108,38,187,38,111-38,187-38,108,38,187,38,111-38,187-38,109,38,188,38,110-38,187-38,108,38,187,38,111-38,187-38,108,38,187,38,111-38,187-38,108,38,187,38,111-38,187-38,109,38,188,38c0,96,0,612,0,612Z"
+				/>
+			</g>
+
+			<g class="flame3">
+				<path
+					class="flame"
+					d="M-1000,1000V388c86-2,111-38,187-38s108,38,187,38,111-38,187-38,108,38,187,38,111-38,187-38,108,38,187,38,111-38,187-38,109,38,188,38,110-38,187-38,108,38,187,38,111-38,187-38,108,38,187,38,111-38,187-38,108,38,187,38,111-38,187-38,109,38,188,38c0,96,0,612,0,612Z"
+				/>
+				<path
+					class=" flame-mask"
+					d="M-1000,1000V388c86-2,111-38,187-38s108,38,187,38,111-38,187-38,108,38,187,38,111-38,187-38,108,38,187,38,111-38,187-38,109,38,188,38,110-38,187-38,108,38,187,38,111-38,187-38,108,38,187,38,111-38,187-38,108,38,187,38,111-38,187-38,109,38,188,38c0,96,0,612,0,612Z"
+				/>
+			</g>
+			<!-- <rect y="670" height="30" x="0" width="1000" class=""/> -->
+			<!-- <rect y="700" height="500" x="0" width="1000" class="bowl"/> -->
+		</g>
+		<!-- <circle cx="500" cy="500" r="450" class="frame" /> -->
+    
+	</svg>
+	
+	<img src="/images/a-symvol.svg" class='h-40 w-40 -mt-40 mx-auto' alt="">
+	<div class="mx-auto h-full h-screen max-w-3xl rounded-2xl bg-white pt-6">
+		<a name="about" />
+
+		<!-- <h1 class="text-center text-6xl font-black text-black">Made by utopia</h1> -->
+    <div class="p-6 lg:text-center">
+          <span class="mb-2 text-xs font-semibold tracking-widest text-neutral-700 uppercase">Made in utopia</span>
+          <h1 class="mt-4   leading-none tracking-tighter  text-6xl font-black text-black ">Chaos theory</h1>
+          <p class="mt-3 text-base leading-relaxed text-gray-500">
+            Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+          </p>
+          <!-- <div class="mt-6">
+            <a href="#" class="flex items-center justify-center w-full px-10 py-4 text-base font-medium text-center text-white transition duration-500 ease-in-out transform bg-blue-600 rounded-xl hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">Accept </a>
+          </div> -->
+        </div>
+	</div>
 </section>
+
+<style>
+	/* @import url("https://fonts.googleapis.com/css?family=Unica+One"); */
+	@-webkit-keyframes flame {
+		100% {
+			transform: rotate(70deg) translate(0, 0);
+		}
+	}
+	@keyframes flame {
+		100% {
+			transform: rotate(70deg) translate(0, 0);
+		}
+	}
+	@-webkit-keyframes flame-mask {
+		100% {
+			transform: rotate(110deg) translate(0, 250px);
+		}
+	}
+	@keyframes flame-mask {
+		100% {
+			transform: rotate(110deg) translate(0, 250px);
+		}
+	}
+	.flame-mask {
+		transform-origin: 500px 500px;
+		transform: rotate(110deg) translate(375px, 250px);
+		-webkit-animation: flame-mask 1s linear infinite;
+		animation: flame-mask 1s linear infinite;
+		-webkit-animation-duration: 1s;
+		animation-duration: 1s;
+	}
+
+	.hand {
+		fill: #fff;
+		/* stroke: #1f242e; */
+		stroke-width: 10;
+	}
+
+	.flame {
+		transform-origin: 500px 500px;
+		transform: rotate(70deg) translate(375px, 0);
+		-webkit-animation: flame 1s linear infinite;
+		animation: flame 1s linear infinite;
+		fill: #fff;
+	}
+
+	.flame2 {
+		transform: translate(160px, 320px);
+	}
+	.flame2 .flame-mask,
+	.flame2 .flame {
+		-webkit-animation-delay: -0.3s;
+		animation-delay: -0.3s;
+	}
+
+	.flame3 {
+		transform: translate(-160px, 280px);
+	}
+	.flame3 .flame-mask,
+	.flame3 .flame {
+		-webkit-animation-delay: -0.8s;
+		animation-delay: -0.8s;
+	}
+
+	/* .bowl {
+  fill: #5971a6;
+} */
+
+	/* . {
+  fill: #1f242e;
+} */
+
+	.frame {
+		fill: none;
+		/* stroke: #1f242e; */
+		stroke-width: 10;
+	}
+
+	/*Page Setup*/
+	svg {
+		/* width: 80vw; */
+		margin: 0 auto;
+		height: 80vh;
+	}
+	/*
+
+.title {
+  height: 10vh;
+  line-height: 10vh;
+  font-size: 8vh;
+  padding: 2.5vh;
+  margin: 0;
+  color: #f7e3d4;
+  border-top: 1px solid;
+  border-bottom: 1px solid;
+  font-family: "Unica One", cursive;
+} */
+</style>
